3fd71ceeba13ed92d61b989e939deaea095e0763
Filipe Mota <filipera3@gmail.com>
2025-11-18 20:25:56 -0300

    learning how controller works

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::post('/register', function() {
-    return 'Thank you!';
-});
+Route::post('/register', [UserController::class, 'register']);
